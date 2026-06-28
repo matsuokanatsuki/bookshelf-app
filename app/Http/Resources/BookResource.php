7528ac27c6feaa9f8e25e759396938941df3c6d6
matsuoka natsuki <natsumugisunny@yahoo.co.jp>
2026-06-28 13:51:19 +0900

    Add ApiTest

--- app/Http/Resources/BookResource.php
+++ app/Http/Resources/BookResource.php
@@ -22,12 +22,13 @@ class BookResource extends JsonResource
             'published_at' => $this->published_at,
             'description' => $this->description,
             'image_url' => $this->image_url,
-            'genres' => $this->genres->pluck('name'),
-            'average_rating' => $this->reviews()->avg('rating'),
-            'reviews_count' => $this->reviews()->count(),
+            'genres' => $this->genres->pluck('name')->values(),
+            'average_rating' => $this->reviews_avg_rating,
+            'reviews_count' => $this->reviews_count,
             'created_by' => $this->creator ? $this->creator->name : null,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
         ];
     }
+
 }
